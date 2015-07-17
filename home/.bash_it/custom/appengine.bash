86225b66ec65011afce2745d90addb89f6fa48cb
fabito <fuechi@ciandt.com>
2015-07-17 12:31:36 -0300

    adjusting gradle and appengine paths

--- home/.bash_it/custom/appengine.bash
+++ home/.bash_it/custom/appengine.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-APPENGINE_PYTHON_SDK=/usr/local/google_appengine
+APPENGINE_PYTHON_SDK=/shared/dev/appengine-sdks/python/latest
 export PATH=$APPENGINE_PYTHON_SDK:$PATH
--- home/.bash_it/custom/gradle.bash
+++ home/.bash_it/custom/gradle.bash
@@ -1,4 +1,4 @@
-GRADLE_HOME=/data/dev/tools/gradle-2.0
+GRADLE_HOME=/shared/dev/gradle/gradle-2.5
 export GRADLE_HOME
 PATH=$PATH:$GRADLE_HOME/bin
 export PATH
